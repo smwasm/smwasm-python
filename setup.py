f70c6f355448fe225d7231f023aae043a6a1a9c4
jzr061 <jzr061@163.com>
2025-02-25 16:14:05 +0800

    update

--- setup.py
+++ setup.py
@@ -1,17 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-PROJECT_URLS = {
-   'Source Code': 'https://github.com/yaml/pyyaml',
-}
-
-setup(
-    name="smwasm",
-    version="0.0.4",
-    author="smwasm",
-    packages=find_packages(),
-    zip_safe=False,
-    include_package_data=True,
-    python_requires=">=3.6",
-    
-    project_urls=PROJECT_URLS,
-)
+setup()
